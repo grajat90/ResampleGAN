0341c68d44f66d7de0c217ccc109231bf954d31e
Rajat Goel <grajat90@gmail.com>
2020-11-17 09:56:40 +0530

    VM_UPDATE

--- start.sh
+++ start.sh
@@ -3,14 +3,14 @@ curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_
 sudo mv cuda-ubuntu1604.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/ /"
-wget https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda_11.1.1_455.32.00_linux.run
-sudo sh cuda_11.1.1_455.32.00_linux.run
-
 sudo apt update
 sudo apt install cuda
+pip list -o --format columns|  cut -d' ' -f1|xargs -n1 pip install -U
+pip install matplotlib
+pip install tqdm
 export model=dense
 export iters=1000
-
 nvidia-smi > nvidia_output.txt
-
-python main.py
+git clone https://github.com/grajat90/ResampleGAN
+cd ResampleGAN
+python ResampleGAN.py
