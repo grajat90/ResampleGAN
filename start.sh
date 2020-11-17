318af46d5334a81dc0c932d701557be538e83b25
Rajat Goel <grajat90@gmail.com>
2020-11-17 09:37:53 +0530

    VM

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-ubuntu1604.pin
+sudo mv cuda-ubuntu1604.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/ /"
+wget https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda_11.1.1_455.32.00_linux.run
+sudo sh cuda_11.1.1_455.32.00_linux.run
+
+sudo apt update
+sudo apt install cuda
+export model=dense
+export iters=1000
+
+nvidia-smi > nvidia_output.txt
+
+python main.py
